3ffbed05fda8063ef121290bd098b67cec087ecd
mac <mac@pc147.home>
2020-08-17 11:44:45 +0000

    Updates

---
+++ precache-manifest.ba2fa8e41dc8970c9018704c2073041c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "773fbc592f4c4c83362340347fd180a6",
+    "url": "/nounou/index.html"
+  },
+  {
+    "revision": "9a2be1103e5f4f9c9f83",
+    "url": "/nounou/static/css/main.34e67cb6.chunk.css"
+  },
+  {
+    "revision": "5a6d895c72a86670d16f",
+    "url": "/nounou/static/js/2.c6272cde.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/nounou/static/js/2.c6272cde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a2be1103e5f4f9c9f83",
+    "url": "/nounou/static/js/main.e84771f9.chunk.js"
+  },
+  {
+    "revision": "e8026fe2cb96f6db2396",
+    "url": "/nounou/static/js/runtime-main.c8e6450a.js"
+  },
+  {
+    "revision": "ce1a3cc9e385d03ffb17211276032eaa",
+    "url": "/nounou/static/media/contacts.ce1a3cc9.jpg"
+  },
+  {
+    "revision": "f1b3a03c2413cc005b89896d70389a8d",
+    "url": "/nounou/static/media/faq_new.f1b3a03c.png"
+  },
+  {
+    "revision": "122df8f78c7b732923907c2b27e1c1e5",
+    "url": "/nounou/static/media/img-7.122df8f7.jpg"
+  },
+  {
+    "revision": "7e397da9d7d6288bddb9b7f7e4194769",
+    "url": "/nounou/static/media/iphone_new.7e397da9.png"
+  },
+  {
+    "revision": "82a7a6762b9a8d27ba5f82ea30be2316",
+    "url": "/nounou/static/media/logo_nav_new.82a7a676.png"
+  },
+  {
+    "revision": "563369f4ba43edd4182a805320c6f4b2",
+    "url": "/nounou/static/media/s10_new.563369f4.png"
+  },
+  {
+    "revision": "04061427445def5f3cf886f8b6db6aed",
+    "url": "/nounou/static/media/satisfaction.04061427.svg"
+  },
+  {
+    "revision": "c241f97d904b9e0aab8571bbe5b53333",
+    "url": "/nounou/static/media/shield.c241f97d.svg"
+  },
+  {
+    "revision": "8ebeeff37a387ab25e2c8c8cb2606449",
+    "url": "/nounou/static/media/stars.8ebeeff3.svg"
+  }
+]);
